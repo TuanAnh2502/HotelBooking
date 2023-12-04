75cfb7b54e9727d2389e44ddc0c21fff6e1c5a92
DanBuii23 <danbuii2001@gmail.com>
2023-12-04 20:06:46 +0700

    sua 2

--- Controllers/TblKhachSansController.cs
+++ Controllers/TblKhachSansController.cs
@@ -23,7 +23,13 @@ namespace HotelBooking.Controllers
         {
             return View(await _context.TblKhachSans.ToListAsync());
         }
-
+        public IActionResult GetFilteredHotels(int rating)
+        {
+            // Xử lý lọc khách sạn dựa trên rating, và trả về một phần của View
+            var filteredHotels = _context.TblKhachSans.Where(h => h.SDanhgia >= rating).ToList();// Code để lấy danh sách khách sạn theo điều kiện rating
+            
+            return PartialView("_FilteredHotelsPartialView", filteredHotels);
+        }
         // GET: TblKhachSans/Details/5
         public async Task<IActionResult> Details(int? id)
         {
